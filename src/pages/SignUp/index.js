ca4fee986ac07d64df463f292a552ccc384ada69
Gabriel Rodrigues <gbo10rodrigues@gmail.com>
2020-05-02 15:41:28 -0300

    Creating login component

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo-app.svg';
 
 export default function SignUp() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
