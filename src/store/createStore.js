85c51ae2a47c5a88b83848689baa66e023fbc3e7
Gabriel Rodrigues <gbo10rodrigues@gmail.com>
2020-05-03 17:16:05 -0300

    Creeating redux base

---
+++ src/store/createStore.js
@@ -0,0 +1,10 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+
+export default (reducers, midlewares) => {
+  const enhancer =
+    process.env.NODE_ENV === 'development'
+      ? compose(console.tron.createEnhancer(), applyMiddleware(...midlewares))
+      : applyMiddleware(...midlewares);
+
+  return createStore(reducers, enhancer);
+};
