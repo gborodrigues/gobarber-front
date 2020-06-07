827f7ea0d0bb3cb27d5346ff67b18b57a592101f
Gabriel Rodrigues <gbo10rodrigues@gmail.com>
2020-06-07 16:44:05 -0300

    fix: undefined property

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,7 +28,7 @@ function Header() {
             </div>
             <img
               src={
-                profile.avatar.url
+                profile.avatar?.url
                 || 'https://api.adorable.io/avatars/50/abott@adorable.png'
               }
               alt="Gabriel"
